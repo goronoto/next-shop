67138229c12ff0926242e861ac2ae8f527ef4736
goronoto <vadym2varava@gmail.com>
2025-09-09 14:37:40 +0300

    Improving Cart logic

---
+++ prisma/migrations/20250907113053_update_cart_item_relation/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `CartItem` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- AlterTable
+ALTER TABLE "CartItem" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "quantity" SET DEFAULT 1;
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
